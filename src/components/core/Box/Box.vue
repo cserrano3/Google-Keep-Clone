b2a46297936002ef44a705802a181795637672e8
carolina <carolina.f.serrano.l@gmail.com>
2019-03-18 00:22:49 -0300

    Setting dynamic input resize for textarea

--- src/components/core/Box/Box.vue
+++ src/components/core/Box/Box.vue
@@ -1,17 +1,21 @@
 <template>
     <div class="box">
         <div class="box--container content">
-            {{defaultText}}
+            <textarea  ref="textArea" @keyup="increaseSize()" class="box--input" :placeholder="defaultText"></textarea>
         </div>
-        
     </div>
-    
 </template>
 <script>
 export default {
     data() {
         return {
             defaultText: 'Write note...'
+        }
+    },
+    methods: {
+        increaseSize() {
+            this.$refs.textArea.style.height = "1px";
+            this.$refs.textArea.style.height = (this.$refs.textArea.scrollHeight)+"px";
         }
     }
 }
